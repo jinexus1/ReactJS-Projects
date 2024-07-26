a58fc8cf6c22177ba76f03c54a43ba862b80dfc3
jinexus1 <ap2929470@gmail.com>
2024-07-26 10:33:18 +0530

    Random_Quote_Generator

---
+++ random_quote_generator/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css'
+import axios from 'axios';
+class App extends React.Component{
+  state={
+    advice: ''
+  };
+  componentDidMount(){
+    this.fetchAdvice();
+  }
+  fetchAdvice=()=>{
+    axios.get("https://api.adviceslip.com/advice")
+    .then((response)=>{
+      const {advice}=response.data.slip;
+      console.log(advice);
+      this.setState({advice});//Update State porperties in class based objects
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
+  render(){
+    const {advice}=this.state;
+    return (
+      <div className="app">
+        <div className="cart">
+          <h1 className="heading">{advice}</h1>
+        
+        <button className="button" onClick={this.fetchAdvice}>
+          <span>GIVE ME ADVICE</span>
+        </button>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
