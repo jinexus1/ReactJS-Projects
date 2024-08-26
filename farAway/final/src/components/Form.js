1e478907070f125af447280dae9e4de814a91250
jinexus1 <ap2929470@gmail.com>
2024-08-26 15:10:45 +0530

    Form Element

---
+++ farAway/final/src/components/Form.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+//onAddItems is passed as props from App.js as "handleAddItems"
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //to prevent default loading default behavior of from element
+    //this is done to have Single Page Application (SPA)
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {/* //it is converted into number because by default it returns string */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+        //to add number of elements as 20 in the list at max
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
